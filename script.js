39305ae4628d7f7e2bc628d0b172fc99a659c3dc
Bruna Soncini <soncinibruna@gmail.com>
2025-01-24 19:24:18 -0300

    atualização para consumir os endpoints da API

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
-document.querySelector("#push").onclick = function () {
-  const input = document.querySelector("#newtask input");
+const API_URL = "https://elom7p8hib.execute-api.sa-east-1.amazonaws.com/dev/tasks"; // URL base da AWS
+
+// GET
+function fetchTasks() {
   const tasksDiv = document.querySelector("#tasks");
 
-  if (input.value.length == 0) {
-    alert("Digite uma tarefa");
-  } else {
-    tasksDiv.style.display = "block";
-    tasksDiv.innerHTML += `
-          <div class="task">
-              <input type="checkbox" class="task-check">
-              <span id="taskname">${input.value}</span>
-              <button class="delete">
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(tasks => {
+      tasksDiv.innerHTML = "";
+
+      if (tasks.length > 0) {
+        tasksDiv.style.display = "block";
+        tasks.forEach(task => {
+          tasksDiv.innerHTML += `
+            <div class="task">
+              <input type="checkbox" class="task-check" ${task.completed ? "checked" : ""}>
+              <span id="taskname">${task.name}</span>
+              <button class="delete" data-id="${task.id}">
                   <i class="far fa-trash-alt"></i>
               </button>
-          </div>
-      `;
+            </div>
+          `;
+        });
 
-    const currentTasks = document.querySelectorAll(".delete");
-    for (let i = 0; i < currentTasks.length; i++) {
-      currentTasks[i].onclick = function () {
-        this.parentNode.remove();
+        assignDeleteEvents();
+      } else {
+        tasksDiv.style.display = "none";
+      }
+    })
+    .catch(error => console.error("Erro ao buscar tarefas:", error));
+}
 
-        if (tasksDiv.children.length === 0) {
-          tasksDiv.style.display = "none";
-        }
-      };
-    }
+// POST
+document.querySelector("#push").onclick = function () {
+  const input = document.querySelector("#newtask input");
+  const taskName = input.value;
 
-    input.value = "";
+  if (taskName.length === 0) {
+    alert("Digite uma tarefa");
+  } else {
+    fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: taskName,
+        completed: false,
+      }),
+    })
+      .then(response => response.json())
+      .then(() => {
+        fetchTasks();
+        input.value = "";
+      })
+      .catch(error => console.error("Erro ao criar tarefa:", error));
   }
 };
+
+// DELETE
+function assignDeleteEvents() {
+  const deleteButtons = document.querySelectorAll(".delete");
+
+  deleteButtons.forEach(button => {
+    button.onclick = function () {
+      const taskId = this.dataset.id;
+
+      fetch(`${API_URL}?id=${taskId}`, {
+        method: "DELETE",
+      })
+        .then(response => response.json())
+        .then(() => {
+          fetchTasks();
+        })
+        .catch(error => console.error("Erro ao excluir tarefa:", error));
+    };
+  });
+}
+
+fetchTasks();
